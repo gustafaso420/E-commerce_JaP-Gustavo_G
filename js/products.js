a64dfb372945c26747ee8143cb51a01eadec6447
Gustavo González <88441847+gustafaso420@users.noreply.github.com>
2021-08-23 09:34:06 -0300

    Punto II / Entregable I
    Petición a la URL con la colección JSON correspondiente a la sección de "Productos".

--- js/products.js
+++ js/products.js
@@ -1,6 +1,31 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+const productList = document.getElementById("productList");
 
+/*Función que se ejecuta una vez que se haya lanzado el evento de
+que el documento se encuentra cargado, es decir, se encuentran todos los
+elementos HTML presentes.*/
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            const products = resultObj.data;          
+            products.forEach(product => {
+                let item = document.createElement("li");
+                item.innerHTML += `
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" " class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                        <p class="mb-1">` + product.currency + ` ` + product.cost + `</p>
+                    </div>
+                </div>
+                `;
+                productList.appendChild(item);
+            });
+        }
+    })
 });
